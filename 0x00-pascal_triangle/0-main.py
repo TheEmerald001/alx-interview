da31333b736705dc0b698a8775a4ca965fe2d3fe
steve <austines811@gmail.com>
2023-07-27 14:27:41 +0300

    creates a pascal triangle

---
+++ 0x00-pascal_triangle/0-main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(8))
