89cdf6ecc992492b5a763deaf1e62296f166ed63
steve <austines811@gmail.com>
2023-08-17 15:37:59 +0300

    fix regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,17 +5,19 @@ import re
 
 
 def extract_input(input_line):
-    '''Extracts sections of an HTTP request log.'''
-    
+    '''Extracts sections of a line of an HTTP request log.
+    '''
     fp = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
         r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)' )
-    
-    info = { 'status_code': 0, 'file_size': 0,}
-        
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
     log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
     resp_match = re.fullmatch(log_fmt, input_line)
     if resp_match is not None:
@@ -27,8 +29,8 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the statistics of the HTTP request log.'''
-    
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
@@ -37,8 +39,14 @@ def print_statistics(total_file_size, status_codes_stats):
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.'''
-    
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
     if status_code in status_codes_stats.keys():
@@ -47,12 +55,19 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    '''Starts the log parser.'''
-    
+    '''Starts the log parser.
+    '''
     line_num = 0
     total_file_size = 0
-    status_codes_stats = { '200': 0, '301': 0, '400': 0,
-        '401': 0, '403': 0, '404': 0, '405': 0, '500': 0,
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
     }
     try:
         while True:
