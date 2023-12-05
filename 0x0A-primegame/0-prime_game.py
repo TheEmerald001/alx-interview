3d37660327c5523d4d071c55a106d56b1adbcb33
steve <austines811@gmail.com>
2023-12-05 08:41:42 +0300

    fix vars

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,17 +15,14 @@ def prime_numbers_between(n):
     Returns:
         int: the number of prime numbers between 1 and n
     """
-    prime_numbers = 0
-
-    for i in range(2, n + 1):
-        is_prime = True
-        for j in range(2, i // 2 + 1):
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            prime_numbers += 1
-    return prime_numbers
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
@@ -39,17 +36,17 @@ def isWinner(x, nums):
     Returns:
         string: the winner of the game (Ben or Maria)
     """
-    if not x or not nums:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    ben = 0
-    maria = 0
+    Maria = Ben = 0
     for i in range(x):
-        prime_nums = prime_numbers_between(nums[i])
-        if prime_nums % 2 == 0:
-            ben += 1
+        prime = prime_numbers_between(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
         else:
-            maria += 1
-    if ben == maria:
-        return None
-    winner = "Ben" if ben > maria else "Maria"
-    return winner
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
